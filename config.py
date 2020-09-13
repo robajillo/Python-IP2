8c23601df43c4e5f49e373c865dfb7adc9fc0723
robajillo <rjillo@yahoo.com>
2020-09-13 11:18:06 +0300

    created subclass prodconfig that contains configuration used in production stage

--- config.py
+++ config.py
@@ -4,3 +4,11 @@ class Config:
     NEWS_API_BASE_URL='https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
     NEWS_API_KEY=os.environ.get("NEWS_API_KEY")
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
