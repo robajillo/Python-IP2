7b0717170d28a12699bbdac7ca69c708a1597540
robajillo <rjillo@yahoo.com>
2020-09-13 12:50:11 +0300

    created class articles

--- app/models.py
+++ app/models.py
@@ -10,3 +10,18 @@ class Sources:
         self.category = category
         self.language = language
         self.country = country
+
+class Articles:
+    """ 
+    Articles class to define Article Objects
+    
+    """
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt):
+
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage= urlToImage
+        self.publishedAt = publishedAt
