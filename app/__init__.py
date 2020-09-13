50d34a14beff3a78ef4217b30575688339a84ff8
robajillo <rjillo@yahoo.com>
2020-09-13 11:34:39 +0300

    created a create_app() function that takes configuration setting key as an argument

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 
 bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
 # Initializing application
 app = Flask(__name__)
 
