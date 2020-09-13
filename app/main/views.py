59c1bea32a028cd08207e0703435ce5a439646ea
robajillo <rjillo@yahoo.com>
2020-09-13 13:14:15 +0300

    defined the view function homepage

--- app/main/views.py
+++ app/main/views.py
@@ -3,3 +3,13 @@ from flask import render_template,redirect,request,url_for
 from . import main
 
 @main.route('/')
+def HomePage():
+    """
+    Views thats renders news sources to the home page
+    """
+    general_news = get_sources('general')
+    entertainment_news = get_sources("entertainment")
+    sports_news = get_sources("sports")
+    technology_news = get_sources("technology")
+    return render_template('sources.html',general=general_news,business=business_news,sports=sports_news,technology=technolgy_news )
+
