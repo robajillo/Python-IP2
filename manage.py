5a90dc0fa22c22be750fc1eab290fb5636d8d137
robajillo <rjillo@yahoo.com>
2020-09-13 13:35:13 +0300

    added the main function

--- manage.py
+++ manage.py
@@ -15,3 +15,6 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
