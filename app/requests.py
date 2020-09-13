44bb0749cc88c214248d43eec6a5ee51086dea76
robajillo <rjillo@yahoo.com>
2020-09-13 12:16:22 +0300

    created a configure_request() function

--- app/requests.py
+++ app/requests.py
@@ -6,4 +6,11 @@ api_key = None
 # Getting the movie base url
 base_url = None
 #getting articles url
-art_url = None
+# art_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    
+    
