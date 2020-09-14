d48a7daf6605d6a751a0863ae82a92844de24329
robajillo <rjillo@yahoo.com>
2020-09-14 09:21:56 +0300

    defined a method and created a root to display articles

--- app/main/views.py
+++ app/main/views.py
@@ -13,3 +13,11 @@ def HomePage():
 
     return render_template('sources.html',general=general_news,entertainment=entertainment_news,sports=sports_news )
 
+@main.route('/News-Articles')
+def NewsArticles():
+    """
+    View that would return news articles
+     
+    """
+    technology_articles = get_articles('technology')
+    return render_template('articles.html', tech =technology_articles)
